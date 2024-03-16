872bc4d2059ad4bf7b9a13908656b89f076c97bd
Жозефина- Емилия Хилько <jozystep@gmail.com>
2024-03-16 09:28:10 +0100

    test

---
+++ main.py
@@ -0,0 +1,9 @@
+number = int(input("Введіть число: "))
+if number % 2 == 0:
+    print("this number is paired")
+else:
+    print("this number isnt paired")
+
+number = int(input("Введіть число: "))
+range(input)
+print(range)
